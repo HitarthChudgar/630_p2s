1ef765024efdcbfb57e22b3ac9b6f8f81ae2d30b
patel1643 <parthptl56@gmail.com>
2021-03-03 23:55:08 -0500

    added logo

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img width=150px src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Fake_Taxi_logo.svg/1280px-Fake_Taxi_logo.svg.png" alt="">
+                <img width=150px src="./take .png" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
