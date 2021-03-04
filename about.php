3099d43eb611157bf27c721920e0201b5d278e40
patel1643 <parthptl56@gmail.com>
2021-03-04 00:17:35 -0500

    updated the group info

--- about.php
+++ about.php
@@ -53,7 +53,7 @@
             <!-- Team Member 3 -->
             <div class="col">
                 <div class="card border-0 shadow">
-                    <img class="dps" src="https://dalalkushal.com/picture.JPG" class="card-img-top" alt="...">
+                    <img class="dps" src="./kushal.jpeg" class="card-img-top" alt="...">
                     <div class="card-body text-center">
                         <h5 class="card-title mb-0">Kushal Dalal</h5>
                         <div class="card-text text-black-50">Web Developer</div>
@@ -71,7 +71,7 @@
     <!-- /.container -->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
@@ -79,7 +79,7 @@
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
-</body>
+    </script>-->
+</body> 
 
 </html>
