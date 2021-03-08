4582170368e6725af5cba23ed9cb7db7264bdcdf
patel1643 <parthptl56@gmail.com>
2021-03-08 17:08:11 -0500

    added homepage

--- index.php
+++ index.php
@@ -7,16 +7,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="./index.css">
     <title>Homepage</title>
 </head>
 
 <body>
     <?php include './views/navbar.php';?>
     <!-- Header -->
-    <img style="display: block; margin-top: 6%; margin-left: auto; margin-right: auto;width: 50%;" src="./public/asset/take.png" class="img-fluid" alt="Responsive image">
+    
+    <header>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <!-- Slide One - Set the background image for this slide in the line below -->
+      <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/LAaSoL0LrYs/1920x1080')">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Order TakeTaxi Cab</h2>
+          <p class="lead">First 100 customers get a 20% off use coupon code TAKE20</p>
+        </div>
+      </div>
+      <!-- Slide Two - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/bF2vsubyHcQ/1920x1080')">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Order Flowers</h2>
+          <p class="lead">First 100 customers get a 20% off use coupon code TAKE20</p>
+        </div>
+      </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+  </div>
+</header>
 
+<!-- Page Content -->
+<section class="py-5">
+  <div class="container">
+    <h1 class="display-4">We have the following services active right now!</h1>
+    <div class="order-container">    
+        <button class="btn btn-dark order-button">Order a Cab!</button>
+    <button class="btn btn-dark order-button">Order Flowers!</button>
+</div>
 
+     </div>
+</section>
 </body> 
 
 </html>
